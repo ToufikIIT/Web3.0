aafb25c80bf7aa667b7a4d6a957057059b61c692
Toufik <toufik.haque2021@gmail.com>
2025-12-23 10:38:36 +0530

    implemented welcome section

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,35 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+import forms from "@tailwindcss/forms";
+
+export default {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  mode: "jit",
+  darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      display: ["Open Sans", "sans-serif"],
+      body: ["Open Sans", "sans-serif"],
+    },
+    extend: {
+      screens: {
+        mf: "990px",
+      },
+      keyframes: {
+        "slide-in": {
+          "0%": {
+            transform: "translateX(120%)",
+          },
+          "100%": {
+            transform: "translateX(0%)",
+          },
+        },
+      },
+      animation: {
+        "slide-in": "slide-in 0.5s ease-out",
+      },
+    },
+  },
+  variants: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [forms],
+};
